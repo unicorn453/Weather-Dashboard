c0d4532a5d40be65f1528e51032ad965623c78a0
unicorn453 <kira.ruseva@gmail.com>
2024-01-29 13:01:04 +0200

    added new html structure for the header and container element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,1 @@
-$(document).ready(function () {
-  var gradientColors = "linear-gradient(to right, #a5b1d5, #4669d0, #062a96)";
-  $("#gradient-background").css("background", gradientColors);
-});
+
