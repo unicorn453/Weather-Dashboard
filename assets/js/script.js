43368373f15a7feac2d4dce43e21092dd80ecbd6
unicorn453 <kira.ruseva@gmail.com>
2024-01-31 03:34:58 +0200

    changed code for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $("#search-button").on("click", function () {
   var geocodeURL =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
-    "&limit=5&appid=5a06ce152c88fa8a790870e5beea3c6f";
+    "&limit=6&appid=5a06ce152c88fa8a790870e5beea3c6f";
 
   fetch(geocodeURL)
     .then(function (response) {
@@ -28,17 +28,33 @@ $("#search-button").on("click", function () {
         .then(function (result) {
           var weatherData = result;
           console.log(weatherData);
-          for (var i = 0; i < 5; i++) {
-            var date = new Date(weatherData.list[i].dt * 1000);
-            var cardTitleSelector = ".card-title-" + (i + 1);
-            var tempSelector = ".temp-" + (i + 1);
-            var windSelector = ".wind-" + (i + 1);
-            var humiditySelector = ".humidity-" + (i + 1);
 
-            $(cardTitleSelector).text(date.toLocaleDateString());
-            $(tempSelector).text(
-              "Temperature: " + weatherData.list[i].main.temp + "°C"
+          // Iterate over the list of weather data for 6 days
+          for (var i = 0; i < weatherData.list.length; i += 8) {
+            var date = new Date(weatherData.list[i].dt * 1000);
+            var date6 = new Date(weatherData.list[39].dt * 1000);
+            var cardTitleSelector = ".card-title-" + (i / 8 + 1);
+            var tempSelector = ".temp-" + (i / 8 + 1);
+            var windSelector = ".wind-" + (i / 8 + 1);
+            var humiditySelector = ".humidity-" + (i / 8 + 1);
+            $(".humidity-6").text(
+              "Humidity: " + weatherData.list[39].main.humidity + "%"
             );
+            $(".wind-6").text(
+              "Wind: " + weatherData.list[39].wind.speed + " KPH"
+            );
+            $(".temp-6").text("Temperature: " + tempCelsius6 + "°C");
+            // Convert temperature from Kelvin to Celsius
+            var tempCelsius = (weatherData.list[i].main.temp - 273.15).toFixed(
+              2
+            );
+            $(".card-title-6").text(date6.toLocaleDateString());
+            var tempCelsius6 = (
+              weatherData.list[39].main.temp - 273.15
+            ).toFixed(2);
+            // Update the UI elements with weather data
+            $(cardTitleSelector).text(date.toLocaleDateString());
+            $(tempSelector).text("Temperature: " + tempCelsius + "°C");
             $(windSelector).text(
               "Wind: " + weatherData.list[i].wind.speed + " KPH"
             );
